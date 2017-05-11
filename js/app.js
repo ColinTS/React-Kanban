4f05e156b0206a9d6b28468cd667dc459f41ce6f
ctsygiel <ctsygiel@gmail.com>
2017-05-11 10:23:20 -1000

    refactored to prepare for moving cards between columns

--- js/app.js
+++ js/app.js
@@ -5,15 +5,19 @@ const Card = (props) => (
   <div className = "card">
     <h3>{ props.card.title }</h3>
     <p>{ props.card.priority }</p>
+    <p>{ props.card.status}</p>
     <p>{ props.card.created_by }</p>
     <p>{ props.card.assigned_to }</p>
+    <p>{ props.card.id }</p>
+    <button id="backward">backward</button>
+    <button id="forward" onClick = { props.moveForward() }>forward</button>
   </div>
 );
 
-const CardList = ({ cards }) => (
+const CardList = ({ cards, moveForward }) => (
   <ul>
     { cards
-      .map( card => <Card card={card} /> )
+      .map( card => <Card card={card} moveForward={moveForward} /> )
     }
   </ul>
 );
@@ -24,11 +28,16 @@ class Cards extends React.Component {
 }
 
 class DoneColumn extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render(){
     return (
         <div>
           <div className="done">
             <p>Done</p>
+            <CardList cards={this.props.done} moveForward={this.props.moveForward}></CardList>
           </div>
         </div>
 
@@ -37,11 +46,16 @@ class DoneColumn extends React.Component {
 }
 
 class ProgressColumn extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
   render(){
     return (
         <div>
           <div className="progress">
             <p>In Progress</p>
+            <CardList cards={this.props.progress} moveForward={this.props.moveForward}></CardList>
           </div>
         </div>
     )
@@ -51,15 +65,15 @@ class ProgressColumn extends React.Component {
 class QueueColumn extends React.Component {
   constructor(props){
     super(props);
-
-    };
+  }
 
   render(){
+    console.log(this.props);
     return (
         <div>
           <div className="queue">
             <p>To Do</p>
-            <CardList cards={this.props.cards}></CardList>
+            <CardList cards={this.props.todo} moveForward={this.props.moveForward} ></CardList>
           </div>
         </div>
     )
@@ -74,14 +88,17 @@ class NewCardForm extends React.Component{
     this.state = {
       title: "",
       priority: "",
+      status: "Queue",
       created_by: "",
-      assigned_to: ""
+      assigned_to: "",
+      id: ""
     };
 
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handlePriorityChange = this.handlePriorityChange.bind(this);
     this.handleCreatedByChange = this.handleCreatedByChange.bind(this);
     this.handleAssignedToChange = this.handleAssignedToChange.bind(this);
+    this.handleIdChange = this.handleIdChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
@@ -94,11 +111,15 @@ class NewCardForm extends React.Component{
     const priority = "";
     const created_by = "";
     const assigned_to = "";
+    const id = "";
+
     this.setState({
       title,
       priority,
+      status,
       created_by,
-      assigned_to
+      assigned_to,
+      id
     });
   }
 
@@ -123,6 +144,14 @@ class NewCardForm extends React.Component{
     this.setState({ assigned_to : event.target.value });
   }
 
+  handleIdChange(event) {
+    this.setState({ id : event.target.value });
+  }
+
+  handleStatusChange(event) {
+    this.setState({ status : event.target.value });
+  }
+
   render(){
     return (
       <form onSubmit={this.handleSubmit}>
@@ -137,6 +166,9 @@ class NewCardForm extends React.Component{
         </div>
         <div>
           <input type="text" placeholder="assigned to" onChange={this.handleAssignedToChange} value={this.state.assigned_to} />
+        </div>
+        <div>
+          <input type="text" placeholder="id" onChange={this.handleIdChange} value={this.state.id} />
         </div>
         <div>
           <button type="submit">Add Card</button>
@@ -155,27 +187,69 @@ class App extends React.Component{
     super(props);
 
     this.state = {
-      cards : [],
-      todo : []
+      cards: [],
+      todo: [],
+      progress: [],
+      done: []
     };
 
     this.addCard = this.addCard.bind(this);
+    this.moveForward = this.moveForward.bind(this);
   }
 
   addCard(card){
+    console.log(card);
     this.setState({
-      cards : this.state.cards.concat(card)
+      todo : this.state.todo.concat(card)
     });
   }
 
+  // let remove = this.state.cards.slice(indexOf(card.id));
+  // let add = this.state.progress.concat(card);
+
+  moveForward(card){
+    console.log('moveForward')
+    // this.setState({
+
+    // });
+
+  }
+
+  componentWillMount() {
+    console.log('hey');
+    const todo = [];
+    const progress = [];
+    const done = [];
+    let allCards = this.state.cards;
+
+     for(var i = 0; i<allCards.length; i++){
+      if (allCards[i].status === "Queue"){
+        todo.push(allCards[i]);
+      }
+      if (allCards[i].status === "In Progress"){
+        progress.push(allCards[i]);
+      }
+      if (allCards[i].status === "Complete"){
+        done.push(allCards[i]);
+      }
+     }
+
+     this.setState({
+      todo: todo,
+      progress: progress,
+      done: done
+    })
+  }
+
   render(){
+
     return (
       <div>
         <h1>Kanban Board</h1>
         <NewCardForm addCard={this.addCard}/>
-        <QueueColumn cards={this.state.cards} />
-        <ProgressColumn />
-        <DoneColumn />
+        <QueueColumn todo={this.state.todo} moveForward={this.moveForward} />
+        <ProgressColumn progress={this.state.progress} moveForward={this.moveForward} />
+        <DoneColumn done={this.state.done} moveForward={this.moveForward} />
       </div>
     );
   }
